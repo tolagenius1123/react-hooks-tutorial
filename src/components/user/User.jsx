f598d410340d0ef2f2a406e9a32668369b223e80
Omotola Jinadu <91120863+tolagenius1123@users.noreply.github.com>
2024-07-18 18:55:47 +0100

    reviewed assignment

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -3,107 +3,105 @@ import ProfilePic from "../../assets/lady.jpg";
 import styles from "./User.module.css";
 
 const User = () => {
-  const [firstName, setFirstName] = useState("Omotola");
-  const [details, setDetails] = useState({
-    firstName: "Omotola",
-    lastName: "Jinadu",
-    email: "tolajinadu1123",
-    age: 40,
-    profession: "Software Engineer",
-  });
-  const [userInput, setUserInput] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    age: "",
-    profession: "",
-  });
-  console.log(userInput);
-  return (
-    <>
-      <div className={styles.user}>
-        <h1 className={styles.heading}>Your Profile</h1>
-        <img className={styles.pic} src={ProfilePic} alt="profile-pic" />
-        <p className={styles.details}>Firstname: {details.firstName}</p>
-        <p className={styles.details}>Lastname: {details.lastName}</p>
-        <p className={styles.details}>Email: {details.email}</p>
-        <p className={styles.details}>Age: {details.age}</p>
-        <p className={styles.details}>Profession: {details.profession}</p>
-      </div>
-      <form
-        className={styles.form}
-        onSubmit={(event) => {
-          e.preventDefault();
-          setDetails(userInput);
-          setUserInput({
-            firstName: "",
-            lastName: "",
-            email: "",
-            age: "",
-            profession: "",
-          });
-        }}
-      >
-        <h1>Update Profile</h1>
-        <div className={styles.field}>
-          <label htmlFor="firstName">Firstname</label>
-          <input
-            type="text"
-            name="firstName"
-            value={userInput.firstName}
-            onChange={(event) =>
-              setUserInput({ ...userInput, firstName: event.target.value })
-            }
-          />
-        </div>
-        <div className={styles.field}>
-          <label htmlFor="lastName">Lastname</label>
-          <input
-            type="text"
-            name="lastName"
-            value={userInput.lastName}
-            onChange={(event) =>
-              setUserInput({ ...userInput, lastName: event.target.value })
-            }
-          />
-        </div>
-        <div className={styles.field}>
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            name="email"
-            value={userInput.email}
-            onChange={(event) =>
-              setUserInput({ ...userInput, email: event.target.value })
-            }
-          />
-        </div>
-        <div className={styles.field}>
-          <label htmlFor="email">Age</label>
-          <input
-            type="number"
-            name="email"
-            value={userInput.age}
-            onChange={(event) =>
-              setUserInput({ ...userInput, age: event.target.value })
-            }
-          />
-        </div>
-        <div className={styles.field}>
-          <label htmlFor="email">Profession</label>
-          <input
-            type="text"
-            name="email"
-            value={userInput.profession}
-            onChange={(event) =>
-              setUserInput({ ...userInput, profession: event.target.value })
-            }
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </>
-  );
+	const [firstName, setFirstName] = useState("Omotola");
+	const [lastName, setLastName] = useState("Jinadu");
+	const [email, setEmail] = useState("tolajinadu1123@gmail.com");
+	const [age, setAge] = useState("40");
+	const [profession, setProfession] = useState("Software Engineer");
+
+	const [updatedFirstName, setUpdatedFirstName] = useState("");
+	const [updatedLastName, setUpdatedLastName] = useState("");
+	const [updatedEmail, setUpdatedEmail] = useState("");
+	const [updatedAge, setUpdatedAge] = useState("");
+	const [updatedProfession, setUpdatedProfession] = useState("");
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		setFirstName(updatedFirstName);
+		setLastName(updatedLastName);
+		setEmail(updatedEmail);
+		setAge(updatedAge);
+		setProfession(updatedProfession);
+
+		// I want to see a notification that tells me profile has been updated successfully here
+
+		// I want you to clear the form for me here
+	};
+
+	return (
+		<>
+			<div className={styles.user}>
+				<h1 className={styles.heading}>Your Profile</h1>
+				<img
+					className={styles.pic}
+					src={ProfilePic}
+					alt="profile-pic"
+				/>
+				<p className={styles.details}>Firstname: {firstName}</p>
+				<p className={styles.details}>Lastname: {lastName}</p>
+				<p className={styles.details}>Email: {email}</p>
+				<p className={styles.details}>Age: {age}</p>
+				<p className={styles.details}>Profession: {profession}</p>
+			</div>
+			<form className={styles.form} onSubmit={handleSubmit}>
+				<h1>Update Profile</h1>
+				<div className={styles.field}>
+					<label htmlFor="firstName">Firstname</label>
+					<input
+						type="text"
+						name="firstName"
+						value={updatedFirstName}
+						onChange={(event) =>
+							setUpdatedFirstName(event.target.value)
+						}
+					/>
+				</div>
+				<div className={styles.field}>
+					<label htmlFor="lastName">Lastname</label>
+					<input
+						type="text"
+						name="lastName"
+						value={updatedLastName}
+						onChange={(event) =>
+							setUpdatedLastName(event.target.value)
+						}
+					/>
+				</div>
+				<div className={styles.field}>
+					<label htmlFor="email">Email</label>
+					<input
+						type="text"
+						name="email"
+						value={updatedEmail}
+						onChange={(event) =>
+							setUpdatedEmail(event.target.value)
+						}
+					/>
+				</div>
+				<div className={styles.field}>
+					<label htmlFor="age">Age</label>
+					<input
+						type="number"
+						name="age"
+						value={updatedAge}
+						onChange={(event) => setUpdatedAge(event.target.value)}
+					/>
+				</div>
+				<div className={styles.field}>
+					<label htmlFor="profession">Profession</label>
+					<input
+						type="text"
+						name="profession"
+						value={updatedProfession}
+						onChange={(event) =>
+							setUpdatedProfession(event.target.value)
+						}
+					/>
+				</div>
+				<button type="submit">Submit</button>
+			</form>
+		</>
+	);
 };
 
 export default User;
