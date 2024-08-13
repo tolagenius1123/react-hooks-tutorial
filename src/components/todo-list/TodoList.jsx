5693cac5c4904d9dff6525fb8406bf14dcafc2c4
Omotola Jinadu <91120863+tolagenius1123@users.noreply.github.com>
2024-08-13 19:58:59 +0100

    edit functionality on todolist

--- src/components/todo-list/TodoList.jsx
+++ src/components/todo-list/TodoList.jsx
@@ -2,45 +2,54 @@ import { useState } from "react";
 import "./TodoList.css";
 
 const TodoList = () => {
-  const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState("");
+	const [todos, setTodos] = useState([]);
+	const [todo, setTodo] = useState("");
 
-  const deleteTodo = () => {
+	const deleteTodo = (id) => {
+		const newTodos = todos.filter((todo) => todo.id !== id);
+		console.log(newTodos);
+		setTodos(newTodos);
+	};
 
-  }
+	const handleAddTodo = (event) => {
+		event.preventDefault();
+		const newTodo = {
+			id: Math.random(),
+			text: todo,
+		};
 
-  const handleAddTodo = (event) => {
-    event.preventDefault();
-    if (todo.trim() !== "") {
-      setTodos([...todos, todo]);
-      setTodo("");
-    }
-  };
-  console.log(todos);
-  return (
-    <div>
-      <h2>What are you working on Today?</h2>
-      <div className="todo-page">
-        <input
-          type="text"
-          name="todo"
-          id="todo"
-          value={todo}
-          placeholder="type your todos here..."
-          onChange={(e) => setTodo(e.target.value)}
-        />
-        <button onClick={handleAddTodo} type="submit">
-          Add
-        </button>
-      </div>
-      {todos.map((todo, index) => (
-        <div className="todos" key={index}>
-          <div>{todo}</div>
-		  <button>Delete</button>
-        </div>
-      ))}
-    </div>
-  );
+		if (todo.trim() !== "") {
+			setTodos([...todos, newTodo]);
+			setTodo("");
+		}
+	};
+
+	console.log(todos);
+
+	return (
+		<div>
+			<h2>What are you working on Today?</h2>
+			<div className="todo-page">
+				<input
+					type="text"
+					name="todo"
+					id="todo"
+					value={todo}
+					placeholder="type your todos here..."
+					onChange={(e) => setTodo(e.target.value)}
+				/>
+				<button onClick={handleAddTodo} type="submit">
+					Add
+				</button>
+			</div>
+			{todos.map((todo) => (
+				<div className="todos" key={todo.id}>
+					<div>{todo.text}</div>
+					<button onClick={() => deleteTodo(todo.id)}>Delete</button>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default TodoList;
